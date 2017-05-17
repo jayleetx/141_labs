0225b753d7c325d56caa8d5b3064bc42faa771e2
Jay Lee <jaylee@reed.edu>
2017-05-17 11:49:13 -0700

    Initial commit

---
+++ Vocabulary.R
@@ -0,0 +1,26 @@
+library()
+data()
+str()
+dplyr::glimpse()
+table()
+dataframe$column
+dplyr::filter()
+x %>% y
+droplevels()
+x <- y
+x == y
+mean()
+(x <- y)
+dplyr::mutate()
+ifelse()
+median()
+ggplot()
+aes()
+geom_point()
+geom_bar
+dplyr::count()
+tidyr::spread()
+dplyr::sample_n()
+dplyr::group_by()
+factor()
+c()
